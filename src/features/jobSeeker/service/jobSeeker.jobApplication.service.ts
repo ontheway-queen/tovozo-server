6b2d359eec8a8490bbfd6255a611a8cd4244fb22
mehedihassanm360ict <mehedihassan.m360ict@gmail.com>
2025-06-23 10:25:36 +0600

    modified create job application

--- src/features/jobSeeker/service/jobSeeker.jobApplication.service.ts
+++ src/features/jobSeeker/service/jobSeeker.jobApplication.service.ts
@@ -2,33 +2,35 @@ import { Request } from "express";
 import AbstractServices from "../../../abstract/abstract.service";
 import { ICreateJobApplicationPayload } from "../../../utils/modelTypes/jobApplication/jobApplicationModel.types";
 
-export class JobSeekerJobApplication extends AbstractServices{
-    constructor(){
-        super();
-    }
+export class JobSeekerJobApplication extends AbstractServices {
+	constructor() {
+		super();
+	}
 
-    public createJobApplication = async (req: Request) => {
-  const { job_post_details_id } = req.query;
-  const { user_id } = req.jobSeeker;
+	public createJobApplication = async (req: Request) => {
+		const { job_post_details_id } = req.query;
+		const { user_id } = req.jobSeeker;
 
-    const payload = {
-        job_post_details_id: Number(job_post_details_id),
-        job_seeker_id: user_id,
-    };
+		const payload = {
+			job_post_details_id: Number(job_post_details_id),
+			job_seeker_id: user_id,
+		};
 
-    return await this.db.transaction(async (trx) => {
-    const model = this.Model.jobApplicationModel(trx);
+		return await this.db.transaction(async (trx) => {
+			const model = this.Model.jobApplicationModel(trx);
 
-    const res = await model.createJobApplication(payload as ICreateJobApplicationPayload);
+			const res = await model.createJobApplication(
+				payload as ICreateJobApplicationPayload
+			);
 
-    await model.markJobPostDetailAsApplied(Number(job_post_details_id));
+			await model.markJobPostDetailAsApplied(Number(job_post_details_id));
 
-    return {
-      success: true,
-      message: this.ResMsg.HTTP_SUCCESSFUL,
-      code: this.StatusCode.HTTP_SUCCESSFUL,
-      data: res[0]?.id,
-    };
-  });
-};
+			return {
+				success: true,
+				message: this.ResMsg.HTTP_SUCCESSFUL,
+				code: this.StatusCode.HTTP_SUCCESSFUL,
+				data: res[0]?.id,
+			};
+		});
+	};
 }
