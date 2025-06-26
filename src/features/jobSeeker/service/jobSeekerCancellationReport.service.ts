f5693e702129e69d8288e098995c7a98410381e2
mehedihassanm360ict <mehedihassan.m360ict@gmail.com>
2025-06-26 18:16:15 +0600

    cancellation application getting for jobseeker

---
+++ src/features/jobSeeker/service/jobSeekerCancellationReport.service.ts
@@ -0,0 +1,57 @@
+import { Request } from "express";
+import AbstractServices from "../../../abstract/abstract.service";
+import { IGetJobPostListParams } from "../../../utils/modelTypes/hotelier/jobPostModelTYpes";
+import { IGetReportsQuery } from "../../../utils/modelTypes/cancellationReport/cancellationReport.types";
+import { REPORT_TYPE } from "../../../utils/miscellaneous/constants";
+
+export class JobSeekerCancellationReportServices extends AbstractServices {
+	constructor() {
+		super();
+	}
+
+	public getCancellationApplicationReports = async (req: Request) => {
+		const { user_id } = req.jobSeeker;
+		const { limit, skip, status } = req.query;
+		const model = this.Model.cancellationReportModel();
+		const data = await model.getJobApplicationReports({
+			user_id,
+			limit,
+			skip,
+			status,
+		} as unknown as IGetReportsQuery);
+		return {
+			success: true,
+			message: this.ResMsg.HTTP_OK,
+			code: this.StatusCode.HTTP_OK,
+			...data,
+		};
+	};
+
+	public getCancellationApplicationReport = async (req: Request) => {
+		const { user_id } = req.jobSeeker;
+		const { id } = req.params;
+
+		const model = this.Model.cancellationReportModel();
+		const data = await model.getSingleJobApplicationReport(
+			Number(id),
+			REPORT_TYPE.CANCEL_APPLICATION,
+			null,
+			user_id
+		);
+
+		if (!data) {
+			return {
+				success: false,
+				message: `Cancellation report with ID ${id} not found`,
+				code: this.StatusCode.HTTP_NOT_FOUND,
+			};
+		}
+
+		return {
+			success: true,
+			message: this.ResMsg.HTTP_OK,
+			code: this.StatusCode.HTTP_OK,
+			data,
+		};
+	};
+}
