3f0cf0ae0adceb3a751ffd48d768b03ddb8622ee
mehedihassanm360ict <mehedihassan.m360ict@gmail.com>
2025-09-21 17:50:34 +0600

    modified

--- dist/utils/miscellaneous/fileFolders.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class FileFolder {
-}
-FileFolder.COMMON_FILES = "common-files";
-FileFolder.JOB_SEEKER_FILES = "job-seeker-files";
-FileFolder.ADMIN_FILES = "admin-files";
-FileFolder.HOTELIER_FILES = "hotelier-files";
-exports.default = FileFolder;
